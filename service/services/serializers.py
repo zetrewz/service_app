cd1f6e1f20552d1359fe495fe4084767703d088d
Magomed Usmanov <mgsmnov@gmail.com>
2024-01-09 13:32:43 +0300

    Day 4 - 1

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,13 +1,24 @@
-from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(ModelSerializer):
-    client_name = serializers.CharField(source='client.company_name')
-    email = serializers.CharField(source='client.user.email')
+    plan = PlanSerializer()
+    client_name = CharField(source='client.company_name')
+    email = CharField(source='client.user.email')
+    price = SerializerMethodField()
+
+    def get_price(self, instance):
+        return (instance.service.full_price -
+                instance.service.full_price * (instance.plan.discount_percent / 100))
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
